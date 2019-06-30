32e5d353ce33a8ff392817cdeb1d37913d71bfe6
Ali Nosrati <ali.nosrati72@gmail.com>
2019-06-30 10:08:27 +0430

    Add support for negative operand

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Input schema:
 	"a + b"
 Acceptable operators:
-	+
+	+ - / *
--- src/test/java/in/nimbo/calculator/BinaryOperationParserTest.java
+++ src/test/java/in/nimbo/calculator/BinaryOperationParserTest.java
@@ -42,4 +42,24 @@ public class BinaryOperationParserTest {
         assertEquals(3, parser.secondNumber);
         assertEquals('/', parser.operator);
     }
+
+
+    @Test
+    public void testNegativeNumberDivideParser(){
+        String input = "-2 / 3";
+        BinaryOperationParser parser = new BinaryOperationParser(input);
+        assertEquals(-2, parser.firstNumber);
+        assertEquals(3, parser.secondNumber);
+        assertEquals('/', parser.operator);
+    }
+
+    @Test
+    public void testTwoNegativeNumberDivideParser(){
+        String input = "-2 / -3";
+        BinaryOperationParser parser = new BinaryOperationParser(input);
+        assertEquals(-2, parser.firstNumber);
+        assertEquals(-3, parser.secondNumber);
+        assertEquals('/', parser.operator);
+    }
+
 }
